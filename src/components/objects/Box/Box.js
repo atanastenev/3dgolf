d5cc5b53e82fda83ce4c5586f12925c38dad33cd
aririggins <66768127+aririggins@users.noreply.github.com>
2022-05-03 21:56:05 -0400

    box collisons

--- src/components/objects/Box/Box.js
+++ src/components/objects/Box/Box.js
@@ -5,13 +5,14 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js';
 
 class Box extends Group {
-    constructor(parent, x, y, z, xSquares, zSquares, xRotation, zRotation) {
+    constructor(parent, x, y, z, xSquares, zSquares) {
         // Call parent Group() constructor
         super();
 
         // size of square
         let squareSize = 2;
-        // checkerboard color
+        //let z = 2;
+        // checkerboard color, testcolor is gray for box
         let materialEven = new THREE.MeshBasicMaterial({color: 0x32a852,});
         let materialOdd = new THREE.MeshBasicMaterial({color: 0x1c802c,});
         let materialTest = new THREE.MeshBasicMaterial({color: 0x808080,});
@@ -51,15 +52,16 @@ class Box extends Group {
         this.box4.position.y = y+1;
         this.box4.position.z = z;
         this.box5.position.x = x;
-        this.box5.position.y = y-1;
-        this.box5.position.z = z;
+        this.box5.position.y = y;
+        this.box5.position.z = z-1;
 
         this.box1.rotation.y = -Math.PI/2;
         this.box3.rotation.y = Math.PI/2;
         this.box4.rotation.x = Math.PI/2;
         this.box4.rotation.y = -Math.PI;
-        this.box5.rotation.x = -Math.PI/2;
-        this.box5.rotation.y = Math.PI;
+        //this.box5.rotation.x = -Math.PI;
+        this.box5.rotation.y = -Math.PI;
+        this.box5.rotation.z = Math.PI/2;
         //this.box3.rotation.x += -Math.PI/2;
         //this.box2.rotation.y = -Math.PI/2;
 
@@ -80,46 +82,11 @@ class Box extends Group {
 
         this.mesh = new THREE.Mesh(fullGeometry, materials);
 
-        // get the corners of plane used to bound
-        // let corners = []
-        // corners.push(new THREE.Vector3(x-xSquares*squareSize/2,y-zSquares*squareSize/2,0))
-        // corners.push(new THREE.Vector3(x-xSquares*squareSize/2,y+zSquares*squareSize/2,0))
-        // corners.push(new THREE.Vector3(x+xSquares*squareSize/2,y-zSquares*squareSize/2,0))
-        // corners.push(new THREE.Vector3(x+xSquares*squareSize/2,y+zSquares*squareSize/2,0))
-
-
-        // rotate plane
-        this.mesh.rotation.x += xRotation;
-        this.mesh.rotation.z += zRotation;
-
-        // get the new normal
-        let xAxis = new THREE.Vector3(1,0,0);
-        let zAxis = new THREE.Vector3(0,0,1);
-        this.normal.applyAxisAngle(xAxis,xRotation);
-        zAxis.applyAxisAngle(xAxis,xRotation);
-        this.normal.applyAxisAngle(zAxis,zRotation);
-
-        //calculate the bounding box
+        //calculate the bounding box for collisons
         let boundingBox = new THREE.Box3();
-        boundingBox.min = new THREE.Vector3(x-1,z-1,y-1);
-        boundingBox.max = new THREE.Vector3(x+1,z+1,y+1);
+        boundingBox.min = new THREE.Vector3(x-1,y-1,z-1);
+        boundingBox.max = new THREE.Vector3(x+1,y+1,z+1);
         this.boundingBox = boundingBox;
-        //this.center = new THREE.Vector3(x,z,y);
-
-
-
-        // get the bounding quad
-        // let quadBound = [];
-        // for(let corner of corners){
-        //     corner.applyAxisAngle(xAxis,xRotation);
-        //     corner.applyAxisAngle(zAxis,zRotation);
-        //     quadBound.push(new THREE.Vector3(corner.x,0,corner.z));
-        // }
-
-        // this.triangleBounds = [];
-        // this.triangleBounds.push(new THREE.Triangle(quadBound[0],quadBound[1],quadBound[2]));
-        // this.triangleBounds.push(new THREE.Triangle(quadBound[1],quadBound[2],quadBound[3]));
-
         
         this.mesh.receiveShadow = true;
 
